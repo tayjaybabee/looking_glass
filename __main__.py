dea040d786d6d158ad2397e784506e81a3bafc5e
Taylor-Jayde Blackstone <tayjaybabee@gmail.com>
2020-02-17 23:05:44 -0600

    Add __main__.py
    Added __main__.py as main app entry point. Still rough. Added argument parsing

--- __main__.py
+++ __main__.py
@@ -6,6 +6,28 @@ class Application:
 
     def __init__(self):
         from argparse import ArgumentParser              # <- To parse cli-arguments
+
+        # Initialize our parser and give our app a description
+        parser = ArgumentParser(prog='LookingGlass',
+                                usage='%(prog)s [options]',
+                                description='LookingGlass gives you a detailed granular look at your home or office\'s'
+                                            ' internal climate while also monitoring localized weather-data. Combine '
+                                            'LookingGlass with your Smart Thermostat and other devices and it will use '
+                                            'collected data to assist you with cost management, maintaining your '
+                                            'comfort, so all you have to focus on is what Inspyres you.',
+                                prefix_chars='`-',
+                                epilog='Inspyre Softworks. run.Everything',
+                                allow_abbrev=True
+                                )
+
+        # Add some arguments for use in our CLI interface
+        parser.add_argument('-v', '--verbose')
+
+        args = parser.parse_args()
+        print(args)
+
+
+
         from lib.common.logger.caller import identify    # <- To ensure a properly formatted logger label/name
         from lib.common.logger import start              # <- Need a logger, and don't wanna disobey D.R.Y.
         import LookingGlass as LookingGlass              # <- Import the actual main script
@@ -14,16 +36,7 @@ class Application:
         log_name = identify('', root=True, root_part=None)
         log = start(log_name)
 
-        # Initialize our parser and give our app a description
-        parser = ArgumentParser(description='LookingGlass gives you a detailed granular look at your home or office\'s'
-                                            ' internal climate while also monitoring localized weather-data. Combine '
-                                            'LookingGlass with your Smart Thermostat and other devices and it will use '
-                                            'collected data to assist you with cost management, maintaining your '
-                                            'comfort, so all you have to focus on is what Inspyres you.'
-                                )
 
-        # Add some arguments for use in our CLI interface
-        parser.add_argument()
 
 
         # Initialize an instance of LookingGlass
